7e680c4ad37a607505ae28e9dc8354cd64b93e5c
gmegh <guillem.megias.homar@gmail.com>
2026-01-13 19:35:29 -0800

    Initial commit

---
+++ python/lsst/transient/finder/__init__.py
@@ -0,0 +1,8 @@
+# This package's namespace is intentionally kept empty, as users typically
+# only need one or two Tasks from it.
+from .findTransients import *
+
+try:
+    from .version import *
+except ImportError:
+    __version__ = "?"
---
+++ python/lsst/transient/finder/findTransients.py
@@ -0,0 +1,232 @@
+# This file is part of ts_wep.
+#
+# Developed for the LSST Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__all__ = [
+    "FindTransientsTaskConnections",
+    "FindTransientsTaskConfig",
+    "FindTransientsTask",
+]
+
+from typing import Any
+from astropy.table import QTable
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+
+import lsst.pipe.base as pipeBase
+from lsst.daf.butler import DataCoordinate
+from lsst.pipe.base import connectionTypes
+
+
+class FindTransientsTaskConnections(
+    pipeBase.PipelineTaskConnections,
+    dimensions=("visit", "instrument"),  # type: ignore
+):
+    detectionCatalogs = connectionTypes.Input(
+        doc="Single Visit Stars Reprocessed Catalogs",
+        dimensions=("visit", "instrument"),
+        storageClass="ArrowAstropy",
+        name="single_visit_star_reprocessed",
+        multiple=True,
+    )
+    transientMatchedCatalogOut = connectionTypes.Output(
+        doc="Transient Catalog with reprocessed matched stars",
+        dimensions=("visit", "instrument"),
+        storageClass="AstropyQTable",
+        name="transient_matched_catalog",
+        multiple=False,
+    )
+    transientUnmatchedCatalogOut = connectionTypes.Output(
+        doc="Transient Catalog with reprocessed unmatched stars",
+        dimensions=("visit", "instrument"),
+        storageClass="AstropyQTable",
+        name="transient_unmatched_catalog",
+        multiple=False,
+    )
+
+    def adjust_all_quanta(self, adjuster: pipeBase.QuantaAdjuster) -> None:
+        """This will drop intra quanta and assign
+        them to the extra detector quanta
+        """
+        consecutive_pair_table = adjuster.butler.get('consecutive_exposure_pairs')
+        to_do = set(adjuster.iter_data_ids())
+        seen = set()
+        while to_do:
+            data_id = to_do.pop()
+            if data_id["visit"] in consecutive_pair_table['visit_id']:
+                seen.add(data_id)
+            elif data_id["visit"] in consecutive_pair_table['prev_visit_id']:
+                row = consecutive_pair_table[consecutive_pair_table["prev_visit_id"] == data_id["visit"]]
+                main_visit_id = DataCoordinate.standardize(
+                    data_id, visit=row['visit_id'].value[0]
+                )
+
+                assert main_visit_id in seen or main_visit_id in to_do, (
+                    f"DataId {main_visit_id} not found in seen or to_do sets."
+                )
+
+                inputs = adjuster.get_inputs(data_id)
+                adjuster.add_input(main_visit_id, "detectionCatalogs", inputs["detectionCatalogs"][0])
+                adjuster.remove_quantum(data_id)
+
+            else:
+                adjuster.remove_quantum(data_id)
+
+
+class FindTransientsTaskConfig(
+    pipeBase.PipelineTaskConfig,
+    pipelineConnections=FindTransientsTaskConnections,  # type: ignore
+):
+    pass
+
+
+class FindTransientsTask(pipeBase.PipelineTask):
+    """
+    Cut out the donut postage stamps on corner wavefront sensors (CWFS)
+    """
+
+    ConfigClass = FindTransientsTaskConfig
+    _DefaultName = "FindTransientsTask"
+    config: FindTransientsTaskConfig
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+    def runQuantum(
+        self,
+        butlerQC: pipeBase.QuantumContext,
+        inputRefs: pipeBase.InputQuantizedConnection,
+        outputRefs: pipeBase.OutputQuantizedConnection,
+    ) -> None:
+        self.log.info("Running FindTransientsTask")
+        flux_unit = u.nJy
+        radius = 1.0 * u.arcsec
+        detection_catalogs = butlerQC.get(inputRefs.detectionCatalogs)
+
+        # We need to ensure we always have the order of visits
+        visits = [ref.dataId["visit"] for ref in inputRefs.detectionCatalogs]
+        if visits[0] > visits[1]:
+            detection_catalogs.reverse()
+            visits.reverse()
+        first_visit, second_visit = visits
+        first_catalog, second_catalog = detection_catalogs
+        self.log.info(f"Processing visits: {first_visit}, {second_visit}")
+
+        first_catalog = first_catalog[~first_catalog["sky_source"]]
+        second_catalog = second_catalog[~second_catalog["sky_source"]]
+        
+        first_catalog = first_catalog[first_catalog["detect_isPrimary"]]
+        second_catalog = second_catalog[second_catalog["detect_isPrimary"]]
+        
+        # Match sky coordinates
+        first_coords = SkyCoord(first_catalog['coord_ra'], first_catalog['coord_dec'], unit='deg')
+        second_coords = SkyCoord(second_catalog['coord_ra'], second_catalog['coord_dec'], unit='deg')
+        idx, sep2d, _ = first_coords.match_to_catalog_sky(second_coords)
+        mask_match = sep2d < radius
+
+        # --- Unmatched stars ---
+        bad_flags = [
+            "pixelFlags_bad",
+            "pixelFlags_cr",
+            "pixelFlags_crCenter",
+            "pixelFlags_edge",
+            "pixelFlags_interpolated",
+            "pixelFlags_interpolatedCenter",
+            "pixelFlags_nodata",
+            "pixelFlags_offimage",
+            "pixelFlags_saturated",
+            "pixelFlags_saturatedCenter",
+            "pixelFlags_suspect",
+            "pixelFlags_suspectCenter",
+            "deblend_skipped",
+        ]
+        def build_good_mask(cat):
+            m = np.ones(len(cat), dtype=bool)
+            for flag in bad_flags:
+                m &= ~cat[flag]
+            return m
+        
+        good_prev = build_good_mask(first_catalog)
+        good_curr = build_good_mask(second_catalog)
+        
+        # --- Matched stars (both matched AND both good) ---
+        matched1 = first_catalog[mask_match]
+        matched2 = second_catalog[idx[mask_match]]
+        
+        good_pair = good_prev[mask_match] & good_curr[idx[mask_match]]
+        
+        matched1 = matched1[good_pair]
+        matched2 = matched2[good_pair]
+        
+        # --- Unmatched stars (unmatched AND good) ---
+        unmatched_prev = first_catalog[(~mask_match) & good_prev]
+        
+        matched_curr_idx = idx[mask_match]
+        is_matched_curr = np.zeros(len(second_catalog), dtype=bool)
+        is_matched_curr[matched_curr_idx] = True
+        
+        unmatched_curr = second_catalog[(~is_matched_curr) & good_curr]
+
+        # butler.get('preliminary_visit_image.mask', instrument='LSSTCam', visit=, detector=)
+        # compute scalar arrays only
+        flux_diff12 = matched2['ap12Flux'] - matched1['ap12Flux']
+        flux_diff09 = matched2['ap09Flux'] - matched1['ap09Flux']
+        flux_diff06 = matched2['ap06Flux'] - matched1['ap06Flux']
+        flux_diffpsf = matched2['psfFlux'] - matched1['psfFlux']
+        extendedness =np.maximum(
+            matched2["sizeExtendedness"],
+            matched1["sizeExtendedness"]
+        )
+
+        matched_table = QTable({
+            "first_visit": first_visit,
+            "second_visit": second_visit,
+            "first_src_id": matched1["sourceId"],
+            "second_src_id": matched2["sourceId"],
+            "flux_diff12": flux_diff12 * flux_unit,
+            "flux_diff09": flux_diff09 * flux_unit,
+            "flux_diff06": flux_diff06 * flux_unit,
+            "flux_diffpsf": flux_diffpsf * flux_unit,
+            "extendedness": extendedness,
+        })
+
+        first_unmatched_table = QTable({
+            "visit": first_visit,
+            "sourceId": unmatched_prev["sourceId"],
+            "ra": unmatched_prev["coord_ra"] * u.deg,
+            "dec": unmatched_prev["coord_dec"] * u.deg,
+            "ap12Flux": unmatched_prev["ap12Flux"] * u.nJy,
+            "extendedness": unmatched_prev["sizeExtendedness"],
+            "which": "prev",
+        })
+
+        second_unmatched_table = QTable({
+            "visit": second_visit,
+            "sourceId": unmatched_curr["sourceId"],
+            "ra": unmatched_curr["coord_ra"] * u.deg,
+            "dec": unmatched_curr["coord_dec"] * u.deg,
+            "ap12Flux": unmatched_curr["ap12Flux"] * u.nJy,
+            "extendedness": unmatched_curr["sizeExtendedness"],
+            "which": "current",
+        })
+        unmatched_table = vstack([first_unmatched_table, second_unmatched_table])
+
+        butlerQC.put(unmatched_table, outputRefs.transientUnmatchedCatalogOut)
+        butlerQC.put(matched_table, outputRefs.transientMatchedCatalogOut)
